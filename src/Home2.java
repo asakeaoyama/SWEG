63b6637ae2129a2ac5cb2961fb4682a0fa9fb0ca
kiwiiiiiiiiO <94119970+kiwiiiiiiiiO@users.noreply.github.com>
2022-01-06 01:52:44 +0800

    Add files via upload

---
+++ src/Home2.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+public class Home2 extends JFrame implements ActionListener{
+	String UserAccountthis;
+	String UserPasswordthis;
+	static String UserAccount,UserPassword;
+	Home2(String UserAccount,String UserPassword){
+		UserAccountthis=UserAccount;
+		UserPasswordthis=UserPassword;
+		//Button
+		JButton Leave = new JButton("Leave");
+		Leave.setBounds(10,20,80,25);
+		JButton Edit = new JButton("Edit");
+		Edit.setBounds(10,20,80,25);
+		//監聽
+		Leave.addActionListener(this);
+		Edit.addActionListener(this);
+		//Panel
+		JPanel Panel = new JPanel();
+		
+		Panel.add(Leave);
+		Panel.add(Edit);
+		setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);  
+		add(Panel);
+		setTitle("Welcome H2");  
+	    setSize(400, 200);  
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		try {
+			String buttonName= e.getActionCommand();
+			if(buttonName.equals("Leave")) {
+				JOptionPane.showMessageDialog(this, "Leaving");
+			}
+			else  {
+				JOptionPane.showMessageDialog(this, "Edit");
+			}
+		}catch(Exception e10) {
+			e10.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		new Home2(UserAccount,UserPassword);	
+	}
+}
